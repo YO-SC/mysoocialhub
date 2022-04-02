c3f3202b4dce290ead8098e619eab973e4d8d2af
Ysael Saez <ysael.saez@outlook.com>
2022-04-02 16:31:10 -0400

    setup grid view for social media hubs page

--- pages/hub/[userId].js
+++ pages/hub/[userId].js
@@ -1,9 +1,22 @@
 import { useRouter } from 'next/router';
+import styles from '../../styles/Hub.module.css';
 
 export default function Hub() {
   // TODO use userId to fetch users hub data on future implementation
   const router = useRouter();
   const { userId } = router.query;
 
-  return <div>The social hub owner is {userId}</div>;
+  return (
+    <div
+      // className={styles.container}
+      className=" bg-red-600 lg:grid"
+    >
+      {/* <h1>The social hub owner is {userId}</h1> */}
+
+      <div className="bg-blue-500 col-start-1 col-end-5">User Info</div>
+      <div className="bg-green-500 col-start-5 col-end-12">
+        Social Media Hubs
+      </div>
+    </div>
+  );
 }
