890178d26227fadc67c0a668bacddc3887126b17
Ysael Saez <ysael.saez@outlook.com>
2022-04-04 14:56:53 -0400

    WIP create social media card and use react icons

--- pages/hub/[userId].js
+++ pages/hub/[userId].js
@@ -1,6 +1,7 @@
 import { useRouter } from 'next/router';
 import ProfilePicture from '../../components/ProfilePicture';
 import styles from '../../styles/Hub.module.css';
+import { FaSmile, FaYoutube, FaFacebook } from 'react-icons/fa';
 
 export default function Hub() {
   // TODO use userId to fetch users hub data on future implementation
@@ -23,16 +24,33 @@ export default function Hub() {
 
         <div className="bg-pink-500 h-full w-3/4 self-center break-words">
           <h1 className="font-bold text-2xl">Username</h1>
-          <h3 className="text-md">Description/About</h3>
+          <h2 className="text-md">Description/About</h2>
         </div>
       </section>
 
       {/* social medias */}
       {/* // NOTE this section should have a grid that takes into account how many social medias the user has */}
       <section className="bg-green-500 col-start-3 col-end-12 p-4 grid gap-4 overflow-y-scroll">
-        {/* // TODO if just 1 social media, apply flex-1, if not then organize them */}
-        <div className="bg-purple-600 p-8 rounded-xl">Social Media Card</div>
+        {/* // NOTE think about how to dynamically set / pass the icon component based on DB data */}
+        <SocialMediaCard icon={<FaYoutube />} name="YouTube" />
       </section>
     </main>
   );
 }
+
+const SocialMediaCard = ({
+  icon = <FaSmile />,
+  name = 'Social Media Name',
+  url = '#',
+}) => {
+  return (
+    <a
+      className="bg-purple-600 p-8 rounded-xl"
+      href={'https://www.youtube.com/channel/UCvjLVsW-VlUyAssyS3QsRQA'}
+      target="_blank"
+    >
+      <h3>{name}</h3>
+      {icon}
+    </a>
+  );
+};
