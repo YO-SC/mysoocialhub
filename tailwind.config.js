4b411a833648868ec362cf2a945c74b112ca3d9c
Ysael Saez <ysael.saez@outlook.com>
2022-04-04 16:01:34 -0400

    add main theme colors to tw config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#F8FFF5',
+        secondary: '#FFF5FF',
+        'accent-primary': '#97B388',
+        'accent-secondary': '#B39AB3',
+      },
+    },
   },
   plugins: [],
-}
+};
