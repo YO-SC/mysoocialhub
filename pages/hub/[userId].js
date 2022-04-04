e0ab18d59dac852a57e10810e53ddd5c4acb1c24
Ysael Saez <ysael.saez@outlook.com>
2022-04-04 16:01:49 -0400

    finish mvp social media cards

--- pages/hub/[userId].js
+++ pages/hub/[userId].js
@@ -1,7 +1,13 @@
 import { useRouter } from 'next/router';
 import ProfilePicture from '../../components/ProfilePicture';
 import styles from '../../styles/Hub.module.css';
-import { FaSmile, FaYoutube, FaFacebook } from 'react-icons/fa';
+import {
+  FaSmile,
+  FaYoutube,
+  FaFacebook,
+  FaTiktok,
+  FaInstagram,
+} from 'react-icons/fa';
 
 export default function Hub() {
   // TODO use userId to fetch users hub data on future implementation
@@ -9,10 +15,10 @@ export default function Hub() {
   const { userId } = router.query;
 
   return (
-    <main className=" bg-red-600 lg:grid h-screen">
+    <main className="  lg:grid h-screen">
       {/* user info */}
-      <section className="bg-blue-500 col-start-1 col-end-3 flex flex-col p-4 gap-4">
-        <div className="bg-red-600 w-3/4 self-center">
+      <section className="bg-primary col-start-1 col-end-3 flex flex-col p-4 gap-4">
+        <div className=" w-3/4 self-center">
           <ProfilePicture
             // className="flex-1"
             src="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
@@ -22,7 +28,7 @@ export default function Hub() {
           />
         </div>
 
-        <div className="bg-pink-500 h-full w-3/4 self-center break-words">
+        <div className=" h-full w-3/4 self-center break-words">
           <h1 className="font-bold text-2xl">Username</h1>
           <h2 className="text-md">Description/About</h2>
         </div>
@@ -30,27 +36,48 @@ export default function Hub() {
 
       {/* social medias */}
       {/* // NOTE this section should have a grid that takes into account how many social medias the user has */}
-      <section className="bg-green-500 col-start-3 col-end-12 p-4 grid gap-4 overflow-y-auto">
-        {/* // NOTE think about how to dynamically set / pass the icon component based on DB data */}
-        <SocialMediaCard icon={<FaYoutube />} name="YouTube" />
+      <section className="bg-secondary col-start-3 col-end-12 px-10 py-4 flex flex-col gap-4 overflow-y-auto">
+        {/* // NOTE think about how to dynamically set / pass the icon component based on DB data. This goes for the icon size and color props too */}
+        <SocialMediaCard
+          icon={<FaFacebook size="2.5rem" color="white" />}
+          name="Facebook"
+        />
+        <SocialMediaCard
+          icon={<FaYoutube size="2.5rem" color="white" />}
+          name="YouTube"
+        />
+        <SocialMediaCard
+          icon={<FaTiktok size="2.5rem" color="white" />}
+          name="TikTok"
+        />
+        <SocialMediaCard
+          icon={<FaInstagram size="2.5rem" color="white" />}
+          name="Instagram"
+        />
       </section>
     </main>
   );
 }
 
 const SocialMediaCard = ({
-  icon = <FaSmile />,
+  bgColor = 'bg-accent-primary',
+  icon = <FaSmile size="2.5rem" color="white" />,
   name = 'Social Media Name',
   url = '#',
 }) => {
   return (
-    <a
-      className="bg-purple-600 p-8 rounded-xl"
-      href={'https://www.youtube.com/channel/UCvjLVsW-VlUyAssyS3QsRQA'}
-      target="_blank"
-    >
-      <h3>{name}</h3>
-      {icon}
-    </a>
+    <div className={`${bgColor} p-8 rounded-xl`}>
+      {/* <span> */}
+      <a
+        // w-fit h-fit flex-1 <- if need to dynamically adjust size
+        className="flex gap-3 items-center justify-center"
+        href={'https://www.youtube.com/channel/UCvjLVsW-VlUyAssyS3QsRQA'}
+        target="_blank"
+      >
+        <span>{icon}</span>
+        <h3 className="text-lg font-semibold text-white">{name}</h3>
+      </a>
+      {/* </span> */}
+    </div>
   );
 };
