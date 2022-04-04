894b9e3fbced8e960f87cc1f18607036ebc6eebf
Ysael Saez <ysael.saez@outlook.com>
2022-04-04 13:31:24 -0400

    fix profile section elements positioning

--- pages/hub/[userId].js
+++ pages/hub/[userId].js
@@ -10,8 +10,8 @@ export default function Hub() {
   return (
     <div className=" bg-red-600 lg:grid h-screen">
       {/* user info */}
-      <div className="bg-blue-500 col-start-1 col-end-5 flex flex-col">
-        <div className="bg-red-600  h-1/2 w-1/2 self-center">
+      <div className="bg-blue-500 col-start-1 col-end-4 flex flex-col p-4 gap-4">
+        <div className="bg-red-600 w-3/4 self-center">
           <ProfilePicture
             // className="flex-1"
             src="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
@@ -21,13 +21,13 @@ export default function Hub() {
           />
         </div>
 
-        <div className="bg-purple-500">
+        <div className="bg-pink-500 h-full w-3/4 self-center">
           <p>info here</p>
         </div>
       </div>
 
       {/* social media hubs */}
-      <div className="bg-green-500 col-start-5 col-end-12">
+      <div className="bg-green-500 col-start-4 col-end-12">
         Social Media Hubs
       </div>
     </div>
