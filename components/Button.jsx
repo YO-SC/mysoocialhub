1f148927f5b0272d68c80e6e87a2914e9d10c1f2
Ysael Saez <ysael.saez@outlook.com>
2022-04-06 16:12:02 -0400

    add color prop to Button component

--- components/Button.jsx
+++ components/Button.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 export default function Button(props) {
   const {
     className = '',
-    bgColor = 'bg-accent-primary',
+    color = 'primary',
+    bgColor = `bg-accent-${color}`,
     text = 'Button',
     textColor = 'text-white',
     onClick = () => console.log('onclick btn event'),
