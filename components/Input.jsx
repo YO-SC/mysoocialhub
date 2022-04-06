91b4c16341cee14bf141c82f80ba49328cf165e4
Ysael Saez <ysael.saez@outlook.com>
2022-04-06 15:51:03 -0400

    create Input component

---
+++ components/Input.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export default function Input(props) {
+  const {
+    className = '',
+    color = 'primary',
+    bgColor = `bg-${color}`,
+    textColor = 'text-black',
+    outline = `outline-accent-${color}`,
+    border = `border-accent-${color} border-2`,
+    type = 'text',
+    placeholder = 'input',
+    name = type,
+    id = type,
+  } = props;
+
+  return (
+    <input
+      className={`${bgColor} px-4 py-3 rounded-xl ${textColor} font-semibold ${outline} ${border} ${className}`}
+      type={type}
+      placeholder={placeholder}
+      name={name}
+      id={id}
+    />
+  );
+}
