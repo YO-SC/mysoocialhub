a85d91713c87d0c4f028221086b796512b1a1bcb
Ysael Saez <ysael.saez@outlook.com>
2022-04-11 17:28:51 -0400

    create and use input states for login

--- pages/login.js
+++ pages/login.js
@@ -1,12 +1,17 @@
 import Head from 'next/head';
-import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
+// import Link from 'next/link';
 import Button from '../components/Button';
 import Input from '../components/Input';
 import { supabase } from '../utils/supabaseClient';
-import { useRouter } from 'next/router';
 
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const router = useRouter();
+  // console.log(email);
+  // console.log(password);
 
   const userLogin = async (email, password) => {
     const { user, error } = await supabase.auth.signIn({
@@ -14,8 +19,13 @@ export default function Login() {
       password,
     });
 
+    // TODO better handle the errors
+    if (error) {
+      return;
+    }
+
     // console.log('the logged in user', user);
-    // console.log('the logged in error', error);
+    // console.error('the logged in error', error);
     const { data: hub, error: errHub } = await supabase
       .from('hub')
       .select('id')
@@ -42,14 +52,25 @@ export default function Login() {
       <section className="bg-primary col-span-6 grid place-items-center p-10">
         <div className="flex flex-col gap-3 w-3/4">
           <h1 className="text-3xl font-bold uppercase mb-2">Login</h1>
-          <Input color="secondary" type="email" placeholder="email" />
-          <Input color="secondary" type="password" placeholder="password" />
+          {/* // TODO validate email and password */}
+          <Input
+            color="secondary"
+            type="email"
+            placeholder="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Input
+            color="secondary"
+            type="password"
+            placeholder="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
           {/*  // TODO make link href dynamic upon login, redirect user after login check/mutation */}
           {/* <Link href="/hub/1234"> */}
           <Button
             className="self-end"
             text="login"
-            onClick={() => userLogin('yodofi7423@bamibi.com', '12345678')}
+            onClick={() => userLogin(email, password)}
           />
           {/* </Link> */}
         </div>
