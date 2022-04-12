c140e28ce1f88e97586268ea2652b8d96c448554
Ysael Saez <ysael.saez@outlook.com>
2022-04-12 10:34:07 -0400

    setup email and password states on signup

--- pages/signup.js
+++ pages/signup.js
@@ -3,8 +3,14 @@ import Link from 'next/link';
 import Button from '../components/Button';
 import Input from '../components/Input';
 import { supabase } from '../utils/supabaseClient';
+import { useState } from 'react';
+import { useRouter } from 'next/router';
 
 export default function SignUp() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const router = useRouter();
+
   const userSignUp = async (email, password) => {
     const { user, session, error } = await supabase.auth.signUp({
       email,
@@ -16,6 +22,12 @@ export default function SignUp() {
     console.error('signed up user', error);
 
     // TODO after user signs up, create a hub for them, redirect to it, the user should be able to "fill" the hub with data
+    // flow
+    // user signs up
+    // create blank hub
+    // connect created blank hub to signed up user and viceversa
+    // create blank theme for blank hub
+    // connect theme to hub
   };
 
   return (
@@ -33,14 +45,24 @@ export default function SignUp() {
       <section className="bg-primary col-span-6 grid place-items-center p-10">
         <div className="flex flex-col gap-3 w-3/4">
           <h1 className="text-3xl font-bold uppercase mb-2">Sign Up</h1>
-          <Input color="secondary" type="email" placeholder="email" />
-          <Input color="secondary" type="password" placeholder="password" />
+          <Input
+            color="secondary"
+            type="email"
+            placeholder="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Input
+            color="secondary"
+            type="password"
+            placeholder="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
           {/*  // TODO make link href dynamic upon sign up, redirect user after sign up check/mutation */}
           {/* <Link href="/hub/1234"> */}
           <Button
             className="self-end"
             text="sign up"
-            onClick={() => userSignUp('yodofi7423@bamibi.com', '12345678')}
+            onClick={() => userSignUp(email, password)}
           />
           {/* </Link> */}
         </div>
