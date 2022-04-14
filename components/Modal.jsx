941770840121bf7861a35b1d4a1f54316354906e
Ysael Saez <ysael.saez@outlook.com>
2022-04-14 09:01:58 -0400

    add Modal component with children support

---
+++ components/Modal.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function Modal(props) {
+  const { isOpen = false, className, children } = props;
+
+  return (
+    <div
+      className={`${
+        !isOpen && 'hidden'
+      } absolute top-0 bottom-0 left-0 right-0 z-50 overflow-auto drop-shadow-2xl flex items-center justify-center ${className}`}
+    >
+      {/* dialog */}
+      <div class="p-8 bg-white rounded-xl w-full max-w-md flex-col flex gap-5">
+        {children}
+      </div>
+    </div>
+  );
+}
