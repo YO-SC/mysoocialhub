24f400dd861e188672d1e81364c8814cc15c611b
Ysael Saez <ysael.saez@outlook.com>
2022-04-14 09:32:57 -0400

    fix Modal nextjs warning

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -10,7 +10,7 @@ export default function Modal(props) {
       } absolute top-0 bottom-0 left-0 right-0 z-50 overflow-auto drop-shadow-2xl flex items-center justify-center ${className}`}
     >
       {/* dialog */}
-      <div class="p-8 bg-white rounded-xl w-full max-w-md flex-col flex gap-5">
+      <div className="p-8 bg-white rounded-xl w-full max-w-md flex-col flex gap-5">
         {children}
       </div>
     </div>
