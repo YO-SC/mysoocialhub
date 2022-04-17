703eba53b2556dd5caa0e4335cb314f727a5854d
Ysael Saez <ysael.saez@outlook.com>
2022-04-17 19:21:05 -0400

    add icon support on Button

--- components/Button.jsx
+++ components/Button.jsx
@@ -8,13 +8,17 @@ export default function Button(props) {
     text = 'Button',
     textColor = 'text-white',
     onClick = () => console.log('onclick btn event'),
+    icon = null,
   } = props;
 
   return (
     <button
-      className={`${bgColor} w-fit px-8 py-3 rounded-xl ${textColor} outline-none ${className}`}
+      className={`${bgColor} w-fit px-8 py-3 rounded-xl ${textColor} outline-none ${className} ${
+        icon && 'flex gap-3 items-center'
+      }`}
       onClick={onClick}
     >
+      {icon}
       {text}
     </button>
   );
